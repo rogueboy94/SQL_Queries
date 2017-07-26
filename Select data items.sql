d15e96f878abd0c5290484eae4178c78953b933a
Kurban Mamedkuliyev <rogueboy_94@mail.ru>
2017-07-26 15:21:41 +0300

    Second commit

--- Select data items.sql
+++ Select data items.sql
@@ -1,4 +1,4 @@
-Select * from dbo.Customers, dbo.Purchases 
-where dbo.Purchases.CustomerId = dbo.Customers.CustomerId 
-AND dbo.Purchases.PurchaiseDateTime >= '20170701' AND dbo.Purchases.PurchaiseDateTime <= '20170729' 
-AND dbo.Purchases.ProductName = 'Молоко'
+select * from Customers c where
+ exists(select * from Purchases where CustomerId = c.CustomerId and ProductName = 'Молоко') and
+ not exists(select * from Purchases where CustomerId = c.CustomerId and ProductName = 'Сметана' and
+  PurchaiseDatetime >= '20170701' and PurchaiseDatetime <= '20170730')
